fa233d5e7165db1c1c67648c0e099a911307874d
Daniel Carlson <dancarl758@gmail.com>
2018-01-27 14:47:16 +0100

    completed crud

--- src/controller/restaurant.js
+++ src/controller/restaurant.js
@@ -20,7 +20,7 @@ export default({ config, db }) => {
         });
     });
 
-    // '/v1/restaurant
+    // '/v1/restaurant - Read
     api.get('/', (req, res) => {
         Restaurant.find({}, (err, restaurants) => {
 
@@ -31,7 +31,7 @@ export default({ config, db }) => {
         });
     });
 
-    // 'v1/restaurant/:id'
+    // 'v1/restaurant/:id' - Read 1
     api.get('/:id', (req, res) => {
         Restaurant.findById(req.params.id, (err, restaurant) => {
 
@@ -39,6 +39,34 @@ export default({ config, db }) => {
                 res.send(err);
             }
             res.json(restaurant);
+        });
+    });
+
+    // 'v1/restaurant/:id' - Update
+    api.put('/:id', (req, res) => {
+        Restaurant.findById(req.params.id, (err, restaurant) => {
+            if (err) {
+                res.send(err);
+            }
+            restaurant.name = req.body.name;
+            restaurant.save(err => {
+                if (err) {
+                    res.send(err);
+                }
+                res.json(restaurant);
+            });
+        });
+    });
+
+    // 'v1/restaurant/:id' - delete
+    api.delete('/:id', (req, res) => {
+        Restaurant.remove({
+            _id: req.params.id
+        }, (err, restaurant) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json({ message: "Restaurant successfully removed" });
         });
     });
 
